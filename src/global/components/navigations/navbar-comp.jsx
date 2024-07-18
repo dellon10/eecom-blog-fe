1909b0df3932fac61516dae2ac1c3d426441dacb
dvlboo <kuhhhckr@gmail.com>
2024-07-18 22:00:40 +0700

    fixed : navbar comp

--- src/global/components/navigations/navbar-comp.jsx
+++ src/global/components/navigations/navbar-comp.jsx
@@ -1,16 +1,16 @@
-import { Navbar, TextInput } from "flowbite-react";
-import { HiHome, HiSearch } from "react-icons/hi";
+import { Dropdown, Navbar, TextInput } from "flowbite-react";
+import { HiCollection, HiDocumentSearch, HiHome, HiInformationCircle, HiSearch, HiUserGroup } from "react-icons/hi";
 import { Link } from "react-router-dom";
 
 export default function NavbarComponent() {
   return (
     <>
-      <Navbar className="flex flex-wrap justify-between items-center p-4 bg-red-600">
+      <Navbar className="flex flex-wrap justify-between items-center p-3 bg-red-600">
         <Link to={"/"} className="flex items-center space-x-3 rtl:space-x-reverse text-white">
-          <img src={"/eecom-logo.png"} className="h-14" alt="EECOM LOGO" />
+          <img src={"/eecom-logo.png"} className="h-10 md:h-14" alt="EECOM LOGO" />
           <div className="flex flex-col justify-center">
-            <h1 className="font-semibold text-xl">UKM-FT EECOM</h1>
-            <span>Engineering English Community</span>
+            <h1 className="font-semibold md:text-xl">UKM-FT EECOM</h1>
+            <span className="md:text-lg">Engineering English Community</span>
           </div>
         </Link>
       
@@ -19,12 +19,63 @@ export default function NavbarComponent() {
         </form>
       </Navbar>
 
-      <div className="bg-slate-200 py-3 flex justify-center">
-        <div className="flex space-x-8 text-center font-semibold">
-          <Link to="/" className="text-red-700 flex items-center"><HiHome className="mr-1"/> Home</Link>
-          <Link to="/about" className="text-red-700">About</Link>
-          <Link to="/articles" className="text-red-700">Articles</Link>
-          <Link to="/contact" className="text-red-700">Contact</Link>
+      <div className="bg-slate-200 text-red-700 overflow-auto scrollbar-hide md:flex md:justify-center">
+        <div className="flex space-x-8 text-center font-medium whitespace-nowrap">
+          <Link to="/" className="flex items-center px-3 py-2 hover:bg-red-600 hover:text-white">
+            <HiHome className="mr-1"/>Home
+          </Link>
+
+          <div className="flex items-center px-3 py-2 hover:bg-red-600 hover:text-white">
+            <Dropdown label={<><HiInformationCircle className="mr-1"/>About</>} inline>
+              <Dropdown.Item className="text-red-700">
+                <Link to="/info-1">Organization</Link>
+              </Dropdown.Item>
+              <Dropdown.Item className="text-red-700">
+                <Link to="/info-1">Mars EECOM</Link>
+              </Dropdown.Item>
+            </Dropdown>
+          </div>
+
+          <div className="flex items-center px-3 py-2 hover:bg-red-600 hover:text-white">
+            <Dropdown label={<><HiUserGroup className="mr-1"/>Committee</>} inline>
+              <Dropdown.Item className="text-red-700">
+                <Link to="/info-1">Daily Manager</Link>
+              </Dropdown.Item>
+              <Dropdown.Item className="text-red-700">
+                <Link to="/info-1">Human Resource Development</Link>
+              </Dropdown.Item>
+              <Dropdown.Item className="text-red-700">
+                <Link to="/info-1">Research And Development</Link>
+              </Dropdown.Item>
+              <Dropdown.Item className="text-red-700">
+                <Link to="/info-1">Information And Communication</Link>
+              </Dropdown.Item>
+              <Dropdown.Item className="text-red-700">
+                <Link to="/info-1">Entrepreneurship</Link>
+              </Dropdown.Item>
+            </Dropdown>
+          </div>
+
+          <div className="flex items-center px-3 py-2 hover:bg-red-600 hover:text-white">      
+            <Dropdown label={<><HiDocumentSearch className="mr-1"/>Information</>} inline>
+              <Dropdown.Item className="text-red-700">
+                <Link to="/info-1">All</Link>
+              </Dropdown.Item>
+              <Dropdown.Item className="text-red-700">
+                <Link to="/info-1">News</Link>
+              </Dropdown.Item>
+              <Dropdown.Item className="text-red-700">
+                <Link to="/info-1">Announcement</Link>
+              </Dropdown.Item>
+              <Dropdown.Item className="text-red-700">
+                <Link to="/info-2">Competition</Link>
+              </Dropdown.Item>
+            </Dropdown>
+          </div>
+          
+          <Link to="/" className="flex items-center px-3 py-2 hover:bg-red-600 hover:text-white">
+            <HiCollection className="mr-1"/>Publication
+          </Link>
         </div>
       </div>
     </>
