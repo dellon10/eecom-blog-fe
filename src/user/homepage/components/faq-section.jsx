e1d5dd72d8c8224ca896e4a87cfbc1d3b27865b6
dvlboo <kuhhhckr@gmail.com>
2024-10-03 19:31:00 +0700

    fix : responsive layout home

--- src/user/homepage/components/faq-section.jsx
+++ src/user/homepage/components/faq-section.jsx
@@ -14,13 +14,13 @@ export default function FAQSection() {
         FREQUENTLY ASKED QUESTION
       </h1>
 
-      <div className="flex flex-row mt-10">
+      <div className="flex md:flex-row flex-col mt-10">
         
-        <div className="w-1/2 flex justify-center items-center">
+        <div className="w-full md:w-1/2 flex justify-center items-center">
           <img src={FAQImage} alt="FAQ" className="w-96 h-auto" />
         </div>
 
-        <div className="w-1/2">
+        <div className="w-full md:w-1/2 mt-5 md:mt-0">
           <div
             id="accordion-flush"
             data-accordion="collapse"
@@ -35,7 +35,7 @@ export default function FAQSection() {
                 aria-expanded={openAccordion === 1 ? 'true' : 'false'}
                 onClick={() => toggleAccordion(1)}
               >
-                <span>Apakah Kegiatan di UKM-FT EECOM Selalu Bahasa Inggris?</span>
+                <span className='text-sm md:text-base'>Apakah Kegiatan di UKM-FT EECOM Selalu Bahasa Inggris?</span>
                 <svg
                   data-accordion-icon
                   className={`w-3 h-3 ${openAccordion === 1 ? '' : 'rotate-180'} shrink-0`}
@@ -62,7 +62,7 @@ export default function FAQSection() {
               aria-labelledby="accordion-flush-heading-1"
             >
               <div className="py-5 border-b border-gray-200 dark:border-gray-700">
-                <p className="mb-2 text-gray-500 dark:text-gray-400 text-justify">
+                <p className="mb-2 text-gray-500 dark:text-gray-400 text-justify text-sm md:text-base">
                 Kegiatan UKM-FT EECOM tidak sepenuhnya selalu menggunakan bahasa Inggris. Meskipun tujuan utama adalah untuk meningkatkan kemampuan bahasa Inggris, namun penggunaan bahasa persatuan yaitu bahasa Indonesia perlu dilakukan, terutama untuk mempermudah koordinasi antar sesama, dalam situasi formal maupun informal. Pada setiap kegiatan tetap ditekankan untuk menggunakan bahasa inggris atau setidaknya diselipkan dengan penggunaan kosa kata bahasa inggris untuk melatih keterampilan dari setiap mahasiswa.
                 </p>
               </div>
@@ -76,7 +76,7 @@ export default function FAQSection() {
                 aria-expanded={openAccordion === 2 ? 'true' : 'false'}
                 onClick={() => toggleAccordion(2)}
               >
-                <span>Apakah di UKM-FT EECOM Hanya Belajar Melalui Materi Saja?</span>
+                <span className='text-sm md:text-base'>Apakah di UKM-FT EECOM Hanya Belajar Melalui Materi Saja?</span>
                 <svg
                   data-accordion-icon
                   className={`w-3 h-3 ${openAccordion === 2 ? '' : 'rotate-180'} shrink-0`}
@@ -103,21 +103,21 @@ export default function FAQSection() {
               aria-labelledby="accordion-flush-heading-2"
             >
               <div className="py-5 border-b border-gray-200 dark:border-gray-700">
-                <p className="mb-2 text-gray-500 dark:text-gray-400 text-justify">
+                <p className="mb-2 text-gray-500 dark:text-gray-400 text-justify text-sm md:text-base">
                 UKM-FT EECOM melakukan proses pembelajaran tidak hanya terbatas pada materi ataupun teori. UKM-FT EECOM menggunakan cara pembelajaran yang lebih interaktif dan bervariasi untuk membantu anggota atau pengurus untuk membantu meningkatkan kemampuan berbahasa inggris. Beberapa metode pembelajaran yang diterapkan adalah seperti diskusi antar kelompok, public speaking, debat, game interaktif dan event yang diselenggarakan.
                 </p>
               </div>
             </div>
             
             {/* Accordion Item 3 */}
-            <h2 id="accordion-flush-heading-2">
+            <h2 id="accordion-flush-heading-3">
               <button
                 type="button"
                 className="flex items-center justify-between w-full py-5 font-medium rtl:text-right text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400 gap-3"
                 aria-expanded={openAccordion === 3 ? 'true' : 'false'}
                 onClick={() => toggleAccordion(3)}
               >
-                <span>Mengapa Jargon UKM-FT EECOM Be Enjoy Be Happy?</span>
+                <span className='text-sm md:text-base'>Mengapa Jargon UKM-FT EECOM Be Enjoy Be Happy?</span>
                 <svg
                   data-accordion-icon
                   className={`w-3 h-3 ${openAccordion === 3 ? '' : 'rotate-180'} shrink-0`}
@@ -139,27 +139,27 @@ export default function FAQSection() {
 
             {/* Konten Accordion 3 */}
             <div
-              id="accordion-flush-body-2"
+              id="accordion-flush-body-3"
               className={`${openAccordion === 3 ? '' : 'hidden'}`}
-              aria-labelledby="accordion-flush-heading-2"
+              aria-labelledby="accordion-flush-heading-3"
             >
               <div className="py-5 border-b border-gray-200 dark:border-gray-700">
-                <p className="mb-2 text-gray-500 dark:text-gray-400 text-justify">
+                <p className="mb-2 text-gray-500 dark:text-gray-400 text-justify text-sm md:text-base">
                   Jargon Be Enjoy Be Happy memiliki arti kata nikmatilah dan berbahagialah. Kata tersebut bermakna bahwa ketika beproses dalam UKM-FT EECOM tentu tidak luput dengan namanya belajar bahasa Inggris, yang mana ketika proses tersebut diperlukan agar setiap individu senang dan menikmati saat proses belajar berlangsung supaya motivasi belajar semakin tinggi sehingga output pembelajaran menjadi efektif dan efisien serta tujuan dari organisasi menjadi tercapai.
                 </p>
               </div>
             </div>
 
 
-            {/* Accordion Item 3 */}
-            <h2 id="accordion-flush-heading-2">
+            {/* Accordion Item 4 */}
+            <h2 id="accordion-flush-heading-4">
               <button
                 type="button"
                 className="flex items-center justify-between w-full py-5 font-medium rtl:text-right text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400 gap-3"
                 aria-expanded={openAccordion === 4 ? 'true' : 'false'}
                 onClick={() => toggleAccordion(4)}
               >
-                <span>Kegiatan di UKM-FT EECOM Untuk Member dan Pengurus?</span>
+                <span className='text-sm md:text-base'>Kegiatan di UKM-FT EECOM Untuk Member dan Pengurus?</span>
                 <svg
                   data-accordion-icon
                   className={`w-3 h-3 ${openAccordion === 4 ? '' : 'rotate-180'} shrink-0`}
@@ -179,14 +179,14 @@ export default function FAQSection() {
               </button>
             </h2>
 
-            {/* Konten Accordion 3 */}
+            {/* Konten Accordion 4 */}
             <div
-              id="accordion-flush-body-2"
+              id="accordion-flush-body-4"
               className={`${openAccordion === 4 ? '' : 'hidden'}`}
-              aria-labelledby="accordion-flush-heading-2"
+              aria-labelledby="accordion-flush-heading-4"
             >
               <div className="py-5 border-b border-gray-200 dark:border-gray-700">
-                <p className="mb-2 text-gray-500 dark:text-gray-400 text-justify">
+                <p className="mb-2 text-gray-500 dark:text-gray-400 text-justify text-sm md:text-base">
                   Seluruh rangkaian kegiatan UKM-FT EECOM tidak hanya untuk member terdaftar ataupun pengurus saja, tetapi terbuka juga untuk seluruh mahasiswa fakultas teknik terutama yang memiliki minat terhadap bahasa inggris. Kegiatan yang terbuka untuk keluarga fakultas teknik adalah seperti contohnya seminar, debat, competisi dan lain sebagainya.
                 </p>
               </div>
--- src/user/homepage/components/fun-activity-section.jsx
+++ src/user/homepage/components/fun-activity-section.jsx
@@ -4,42 +4,42 @@ import comperativeStudy from '../assets/comperative-study.jpeg';
 export default function FunActivitySection() {
   return (
     <section className="bg-red-700 md:px-24 px-4 py-7 md:py-20">
-      <h1 className="text-white text-center w-full text-4xl font-bold mb-10">FUN ACTIVITY</h1>
+      <h1 className="text-white text-center w-full text-2xl md:text-4xl font-bold md:mb-10 mb-5">FUN ACTIVITY</h1>
 
       <div className="flex md:flex-row flex-col gap-4">
 
         <div className="flex flex-col md:w-1/2 w-full gap-3">
           
-          <div className="flex flex-row p-8 bg-white rounded-xl justify-between">
-            <div className="flex flex-col w-3/5">
-              <h1 className="text-black text-xl font-semibold">MAROON DAY</h1>
-              <p className="text-black text-base mt-3 text-justify">Maroon Day is a routine agenda of UKM-FT EECOM aimed at improving the English skills of EECOM members and is usually held on Wednesdays.</p>
+          <div className="flex md:flex-row flex-col p-8 bg-white rounded-xl justify-between">
+            <div className="flex flex-col md:w-3/5">
+              <h1 className="text-black text-lg md:text-xl font-semibold">MAROON DAY</h1>
+              <p className="text-black text-sm md:text-base mt-3 text-justify">Maroon Day is a routine agenda of UKM-FT EECOM aimed at improving the English skills of EECOM members and is usually held on Wednesdays.</p>
             </div>
 
-            <div className="w-40 h-40 overflow-hidden rounded-xl">
+            <div className="md:w-40 md:h-40 overflow-hidden rounded-xl mt-5 md:mt-0">
               <img src={maroonDay} alt="" className="object-cover w-full h-full"/>
             </div>
           </div>
           
-          <div className="flex flex-row p-8 bg-white rounded-xl justify-between">
-            <div className="flex flex-col w-3/5">
-              <h1 className="text-black text-xl font-semibold">NUDC TRAINING</h1>
-              <p className="text-black text-base mt-3 text-justify">UKM-FT EECOM organizes training and mentoring for EECOM members who want to participate in the National University Debate Championship (NUDC)</p>
+          <div className="flex md:flex-row flex-col p-8 bg-white rounded-xl justify-between">
+            <div className="flex flex-col md:w-3/5">
+              <h1 className="text-black text-lg md:text-xl font-semibold">NUDC TRAINING</h1>
+              <p className="text-black text-sm md:text-base mt-3 text-justify">UKM-FT EECOM organizes training and mentoring for EECOM members who want to participate in the National University Debate Championship (NUDC)</p>
             </div>
 
-            <div className="w-40 h-40 overflow-hidden rounded-xl">
+            <div className="md:w-40 md:h-40 overflow-hidden rounded-xl mt-5 md:mt-0">
               <img src={maroonDay} alt="" className="object-cover w-full h-full"/>
             </div>
           </div>
 
         </div>
 
-        <div className="flex md:flex-col flex-row bg-white rounded-xl md:w-1/2 w-full p-8 justify-between">
-          <div className='flex flex-col w-3/5 md:w-full text-justify'>
-            <h1 className="text-black text-xl font-semibold">COMPERATIVE STUDY</h1>
-            <p className="text-black text-base mt-3">Comperative Study is an agenda organized by EECOM with the aim of sharing knowledge with external parties that have similar organizational backgrounds and building new connections.</p>
+        <div className="flex flex-col bg-white rounded-xl md:w-1/2 w-full p-8 justify-between">
+          <div className='flex flex-col w-full text-justify'>
+            <h1 className="text-black text-lg md:text-xl font-semibold">COMPERATIVE STUDY</h1>
+            <p className="text-black text-sm md:text-base mt-3">Comperative Study is an agenda organized by EECOM with the aim of sharing knowledge with external parties that have similar organizational backgrounds and building new connections.</p>
           </div>
-          <div className='w-40 h-40 md:w-auto md:h-60 overflow-hidden rounded-xl mt-5'>
+          <div className='w-auto h-60 overflow-hidden rounded-xl mt-5'>
             <img src={comperativeStudy} alt="" className='object-cover w-full h-full'/>
           </div>
         </div>
