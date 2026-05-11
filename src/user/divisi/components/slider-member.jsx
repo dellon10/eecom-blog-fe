78689359777cef1a14819a5b0396e56fea55bd99
Dellon10 <dellonismu10.com>
2026-05-12 02:19:55 +0700

    passin card

--- src/user/divisi/components/slider-member.jsx
+++ src/user/divisi/components/slider-member.jsx
@@ -2,9 +2,9 @@ import PropTypes from 'prop-types';
 import Slider from 'react-slick';
 
 const SliderMember = ({ data }) => {
-  
+
   const settings = {
-    accessibility:true,
+    accessibility: true,
     dots: false,
     infinite: true,
     slidesToShow: 3,
@@ -39,33 +39,58 @@ const SliderMember = ({ data }) => {
       }
     ]
   };
-  
+
   return (
-    <div className='bg-slate-300 py-3 px-2'>
+    <div className="bg-slate-300 py-3 px-2">
       <Slider {...settings}>
-        {data.member.map((member) => (
-          <>
-            <div className="bg-white text-black rounded-lg border border-slate-400">
-              <div className="rounded-t-lg bg-red-500 flex justify-center items-center">
-                <img src={member.photo} alt={member.name} className="h-44 w-44 object-cover rounded-full bg-white"/>
+        {data.member.map((member, index) => (
+          <div key={index} className="px-2 h-full">
+            
+            <div className="bg-white text-black rounded-lg border border-slate-400 h-full flex flex-col shadow-md">
+              
+              {/* FOTO */}
+              <div className="rounded-t-lg bg-red-500 flex justify-center items-center py-4">
+                <img
+                  src={member.photo}
+                  alt={member.name}
+                  className="h-44 w-44 object-cover rounded-full bg-white"
+                />
               </div>
 
-              <div className="flex flex-col justify-center items-center p-4">
-                <h5 className="text-lg font-semibold">{member.name}</h5>
-                <h5 className="text-base font-semibold text-red-600">{member.as}</h5>
-                <h5 className="my-2 text-sm text-center">{member.major}&apos;{member.akt}</h5>
-                <button className="bg-red-500 text-white text-lg px-6 py-1 rounded-xl mt-2">More</button>
+              {/* ISI CARD */}
+              <div className="flex flex-col justify-between items-center p-4 flex-1 text-center">
+                
+                <div>
+                  <h5 className="text-lg font-semibold">
+                    {member.name}
+                  </h5>
+
+                  <h5 className="text-base font-semibold text-red-600">
+                    {member.as}
+                  </h5>
+
+                  <h5 className="my-2 text-sm">
+                    {member.major}&apos;{member.akt}
+                  </h5>
+                </div>
+
+                {/* BUTTON */}
+                <button className="bg-red-500 text-white text-lg px-6 py-1 rounded-xl mt-4 hover:bg-red-600 transition">
+                  More
+                </button>
+
               </div>
             </div>
-          </>
+
+          </div>
         ))}
       </Slider>
     </div>
-  )
-}
+  );
+};
 
 SliderMember.propTypes = {
   data: PropTypes.any.isRequired
-}
+};
 
 export default SliderMember;
